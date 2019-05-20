49fc803dd33407f56d216a775ff308642807e5ff
Camille BRETAGNON <cbretagn@e1r3p1.42.fr>
2019-05-20 17:09:12 +0200

    dynamic arrays tools

--- dynamic.h
+++ dynamic.h
@@ -6,14 +6,15 @@
 /*   By: cbretagn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:00:58 by cbretagn          #+#    #+#             */
-/*   Updated: 2019/05/20 15:11:15 by cbretagn         ###   ########.fr       */
+/*   Updated: 2019/05/20 17:08:28 by cbretagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DYNAMIC_H
 # define DYNAMIC_H
 
-# define BASE_CAP 10
+# define BASE_CAP 	256
+# define BASE_ARRAY	20
 
 typedef struct		s_dstring
 {
@@ -33,4 +34,8 @@ t_dstring			*create_dstring(unsigned int cap, char *str);
 t_dstring			*push_str(t_dstring *dest, char *src);
 void				delete_dstring(t_dstring *del);
 
+t_darray			*create_darray(unsigned int cap);
+void				delete_darray(t_darray *del);
+int					cpy_darray(t_darray *dest, t_darray *src);
+t_darray			*push_dstr(t_darray *dest, t_dstring *src);
 #endif
